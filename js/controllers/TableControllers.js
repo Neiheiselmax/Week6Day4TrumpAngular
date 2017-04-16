c3e9816a25ff3b91c0d696e69b0b1298781f70ff
Max Neiheisel <maxneiheisel@Max-Neiheisels-MacBook-2.local>
2017-04-16 19:32:19 -0400

    added more animation

--- js/controllers/TableControllers.js
+++ js/controllers/TableControllers.js
@@ -7,12 +7,49 @@
 
           const vm = this;
 
-        let data = API.getData();
-        
-        data.then(function successCallback(response){
-            console.log(response)
-            vm.quote = response.data.value
+          let svg = document.querySelector('svg');
 
+
+            let counter = 0;
+            svg.addEventListener('click', e =>{
+
+            if(counter==0)
+            {
+                let face = document.getElementById('layer7');
+                face.classList.add('faceDown');
+            }
+            else if(counter==1)
+            {
+                let leftEye = document.getElementById('layer6');
+                leftEye.classList.add('leftEye');
+            }
+            else if(counter==2)
+            {
+                let rightEye = document.getElementById('layer5');
+                rightEye.classList.add('rightEye');
+            }
+            else if(counter==3)
+            {
+                let nose = document.getElementById('layer4');
+                nose.classList.add('nose');
+            }
+            else if(counter==4)
+            {
+                let hair = document.getElementById('layer2');
+                hair.classList.add('hair');
+            }
+            counter++;
+            let data = API.getData();
+            data.then(function successCallback(response){
+                console.log(response)
+                vm.quote = response.data.value
+            
+            svg.addEventListener('click', e =>{
+            let mouth = document.getElementById('layer3');
+            mouth.classList.add('mouth');
+            
+            })
+            })
         })
 
 
